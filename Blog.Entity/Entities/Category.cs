6c9788029c3ed9590765d219c57aa4fcd1610293
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-02-04 23:24:23 +0300

    Category clası eklendi ve propları oluşturuldu

---
+++ Blog.Entity/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Entity.Entities
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Article> Articles { get; set; }
+    }
+}
