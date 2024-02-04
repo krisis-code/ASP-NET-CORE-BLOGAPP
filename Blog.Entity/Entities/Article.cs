bb4c48419a75ec0e06ee995e664cb0f6cc7d77a0
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-02-04 23:21:48 +0300

    Article entities eklendi ve proplar eklendi

---
+++ Blog.Entity/Entities/Article.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Entity.Entities
+{
+    public class Article
+    {
+
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+        
+        public string Content { get; set; }
+        
+        public int  ViewCount { get; set; }
+
+        public Guid CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        public Guid ImageId { get; set; }
+
+        public Image Image { get; set; }
+
+    }
+}
