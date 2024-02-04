2f2dcbb7620f58f28e451d4fed593a9f9698b148
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-02-04 23:25:54 +0300

    Image clası eklendi ve propları oluşturuldu

---
+++ Blog.Entity/Entities/Image.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Entity.Entities
+{
+    public class Image
+    {
+        public Guid Id { get; set; }
+
+        public string FileName { get; set; }
+    
+        public string FileType { get; set; }
+    
+        public ICollection<Article> Articles { get; set; }
+    }
+}
