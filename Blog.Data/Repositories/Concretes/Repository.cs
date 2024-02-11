93951a31786cc0d5253d8fa7ac075a26dbc218f7
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-02-11 18:03:43 +0300

    Repository clasına metodlar eklendi

--- Blog.Data/Repositories/Concretes/Repository.cs
+++ Blog.Data/Repositories/Concretes/Repository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Blog.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,17 @@ namespace Blog.Data.Repositories.Concretes
 {
     public class Repository
     {
+        private readonly AppDbContext dbContext;
+
+        public Repository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        private DbSet<T> Table { get => dbContext.Set<T>()}
+
+        public async Task addAsync(T entity)
+        {
+            await Table.AddAsync(entity);
+        }
     }
 }
