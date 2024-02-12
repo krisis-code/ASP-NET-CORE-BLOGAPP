16a9b5588a46de47f4d270cdcac10cc08f69d8c8
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-02-12 23:38:38 +0300

    GetAllAsync metodu yazıldı

--- Blog.Data/Repositories/Concretes/Repository.cs
+++ Blog.Data/Repositories/Concretes/Repository.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +20,19 @@ namespace Blog.Data.Repositories.Concretes
         }
         private DbSet<T> Table { get => dbContext.Set<T>();   }
 
+        public async Task<List<T>> GetAllAsync(Expression<Func<T,bool>> predicate = null , params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = Table;
+            if (predicate !=null)
+                query = query.Where(predicate);
+
+            if (includeProperties.Any())
+                foreach (var item in includeProperties)
+                    query = query.Include(item);
+
+            return await query.ToListAsync();
+
+        }
         public async Task addAsync(T entity)
         {
             await Table.AddAsync(entity);
