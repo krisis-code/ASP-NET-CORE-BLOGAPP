fee4481932155d703bbb0512d38f7cebee79322e
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-02-12 23:39:28 +0300

    GetAllAsync İnterface e hali yazıldı

--- Blog.Data/Repositories/Abstractions/IRepository.cs
+++ Blog.Data/Repositories/Abstractions/IRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,6 @@ namespace Blog.Data.Repositories.Abstractions
     public interface IRepository<T> where T : class , IEntityBase, new ()
     {
         Task addAsync(T entity);
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties);
     }
 }
