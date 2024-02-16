94d572df84dd4f5b93903007ca56b8a1a0c654aa
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-02-16 21:10:24 +0300

    DataLayerExtensions clası oluşturuldu ve LoadDataLayerExtension metodu yazıldı

---
+++ Blog.Data/Extensions/DataLayerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Blog.Data.Repositories.Abstractions;
+using Blog.Data.Repositories.Concretes;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Extensions
+{
+    public static class DataLayerExtensions
+    {
+        public static IServiceCollection LoadDataLayerExtension(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            return services;
+        }
+    }
+}
