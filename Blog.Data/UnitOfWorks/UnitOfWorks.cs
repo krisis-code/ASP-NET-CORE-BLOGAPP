2d6ea1b934cc12455c5b23e88deaead0cd836a93
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-02-16 22:00:14 +0300

    UnitOfWorks constructor metodu oluşturuldu ve IUnitOfWorks implamente edilip metodların içi dolduruldu

---
+++ Blog.Data/UnitOfWorks/UnitOfWorks.cs
@@ -0,0 +1,39 @@
+﻿using Blog.Data.Context;
+using Blog.Data.Repositories.Abstractions;
+using Blog.Data.Repositories.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.UnitOfWorks
+{
+    public class UnitOfWorks : IUnitOfWorks
+    {
+        private readonly AppDbContext dbContext;
+        public UnitOfWorks(AppDbContext dbContext)
+        {
+           this.dbContext = dbContext;
+        }
+        public async ValueTask DisposeAsync()
+        {
+            await dbContext.DisposeAsync();
+        }
+
+        public int save()
+        {
+            return dbContext.SaveChanges();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await dbContext.SaveChangesAsync();
+        }
+
+        IRepository<T> IUnitOfWorks.GetRepository<T>()
+        {
+            return new Repository<T>(dbContext);
+        }
+    }
+}
