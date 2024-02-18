a925113bd86699f317e2f41c2d2f8eaec92f1918
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-02-18 23:09:59 +0300

    GetAllArticlesAsync metodu implamente edildi

--- Blog.Service/Services/Concretes/ArticleService.cs
+++ Blog.Service/Services/Concretes/ArticleService.cs
@@ -1,9 +1,13 @@
 ﻿using Blog.Data.Repositories.Abstractions;
+using Blog.Entity.Entities;
 
 namespace Blog.Data.Repositories.Concretes
 {
-    public class ArticleService: IArticleService
+    public class ArticleService : IArticleService
     {
-      
+        public Task<List<Article>> GetAllArticlesAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
