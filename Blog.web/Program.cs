c33bbed3f7dda79a05d0c1d1b64aa93834c0b0a4
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-02-19 22:05:25 +0300

    Admin ekranı için route endpoint eklendi

--- Blog.web/Program.cs
+++ Blog.web/Program.cs
@@ -23,8 +23,14 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapAreaControllerRoute(
+        name: "Admin",
+        areaName: "Admin",
+        pattern: "Admin/{controller=Home}/{action=Index}/{id?}"
+        );
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.Run();
