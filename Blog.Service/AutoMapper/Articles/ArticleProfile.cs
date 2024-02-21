bc46a6764ab8df6dd4b44f416c6af112bfbd62eb
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-02-21 22:26:45 +0300

    Mapleme işlemi yapıldı

--- Blog.Service/AutoMapper/Articles/ArticleProfile.cs
+++ Blog.Service/AutoMapper/Articles/ArticleProfile.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using Blog.Entity.DTOs.Articles;
+using Blog.Entity.Entities;
 
 namespace Blog.Service.AutoMapper.Articles
 {
-    public class ArticleProfile
+    public class ArticleProfile : Profile
     {
+        public ArticleProfile()
+        {
+            CreateMap<ArticleDto,Article>().ReverseMap();
+        }
     }
 }
