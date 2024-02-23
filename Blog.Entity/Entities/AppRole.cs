be22a83e986efb7635b3a334dc876577e9d996b9
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-02-23 18:37:41 +0300

    AppRoler clası eklendi

---
+++ Blog.Entity/Entities/AppRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Entity.Entities
+{
+    public class AppRole
+    {
+
+    }
+}
