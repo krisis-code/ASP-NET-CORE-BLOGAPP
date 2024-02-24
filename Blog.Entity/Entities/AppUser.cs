252cd0b056e8d1a88c56bcfa5ec08c0734170227
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-02-24 21:25:31 +0300

    AppUser clası IdentityUser clasında kalıtıldı  FirstName ve LastName propertyleri eklendi

--- Blog.Entity/Entities/AppUser.cs
+++ Blog.Entity/Entities/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace Blog.Entity.Entities
 {
-    public class AppUser
+    public class AppUser : IdentityUser<Guid>
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
     }
 }
