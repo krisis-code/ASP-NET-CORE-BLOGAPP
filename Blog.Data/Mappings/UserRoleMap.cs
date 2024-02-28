7375dd4476575928e3aeace2822d2aaf273ce8c7
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-02-28 21:43:13 +0300

    UserRoleMap class IEntityTypeConfiguration türetildi ve appuser dan implamente edildi ve configre edildi

--- Blog.Data/Mappings/UserRoleMap.cs
+++ Blog.Data/Mappings/UserRoleMap.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Blog.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,15 @@ using System.Threading.Tasks;
 
 namespace Blog.Data.Mappings
 {
-    public class UserRoleMap
+    public class UserRoleMap : IEntityTypeConfiguration<AppUserRole>
     {
+        public void Configure(EntityTypeBuilder<AppUserRole> builder)
+        {
+            // Primary key
+            builder.HasKey(r => new { r.UserId, r.RoleId });
+
+            // Maps to the AspNetUserRoles table
+            builder.ToTable("AspNetUserRoles");
+        }
     }
 }
