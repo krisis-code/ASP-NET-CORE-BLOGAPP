6687b3ddf6484d24280c4537089279731b6ffdf4
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-02-28 22:18:41 +0300

    Seed data user superadmin ve admin data sı eklendi

--- Blog.Data/Mappings/UserMap.cs
+++ Blog.Data/Mappings/UserMap.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Entity.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
@@ -46,7 +47,48 @@ namespace Blog.Data.Mappings
 
             // Each User can have many entries in the UserRole join table
             builder.HasMany<AppUserRole>().WithOne().HasForeignKey(ur => ur.UserId).IsRequired();
-        
+
+            var superadmin = new AppUser
+            {
+                Id = Guid.Parse("CB94223B-CCB8-4F2F-93D7-0DF96A7F065C"),
+                UserName = "superadmin@gmail.com",
+                NormalizedUserName = "SUPERADMIN@GMAIL.COM",
+                Email = "superadmin@gmail.com",
+                NormalizedEmail = "SUPERADMIN@GMAIL.COM",
+                PhoneNumber = "+905439999999",
+                FirstName = "Cem",
+                LastName = "Keskin",
+                PhoneNumberConfirmed = true,
+                EmailConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                 //= Guid.Parse("F71F4B9A-AA60-461D-B398-DE31001BF214")
+            };
+            superadmin.PasswordHash = CreatePasswordHash(superadmin, "123456");
+
+            var admin = new AppUser
+            {
+                Id = Guid.Parse("3AA42229-1C0F-4630-8C1A-DB879ECD0427"),
+                UserName = "admin@gmail.com",
+                NormalizedUserName = "ADMIN@GMAIL.COM",
+                Email = "admin@gmail.com",
+                NormalizedEmail = "ADMIN@GMAIL.COM",
+                PhoneNumber = "+905439999988",
+                FirstName = "Admin",
+                LastName = "User",
+                PhoneNumberConfirmed = false,
+                EmailConfirmed = false,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                //ImageId = Guid.Parse("D16A6EC7-8C50-4AB0-89A5-02B9A551F0FA")
+            };
+            admin.PasswordHash = CreatePasswordHash(admin, "123456");
+
+            builder.HasData(superadmin, admin);
+
+        }
+        private string CreatePasswordHash(AppUser user, string password)
+        {
+            var passwordHasher = new PasswordHasher<AppUser>();
+            return passwordHasher.HashPassword(user, password);
         }
     }
 }
