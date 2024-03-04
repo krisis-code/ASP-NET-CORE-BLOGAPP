ce7d08ea1e0cd43242aca9abacfa5ff428665b17
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-03-04 22:04:09 +0300

    AppUser clasına ImageId ve  Image propları eklendi article clasıda ICollection olarak eklendi

--- Blog.Entity/Entities/AppUser.cs
+++ Blog.Entity/Entities/AppUser.cs
@@ -12,5 +12,11 @@ namespace Blog.Entity.Entities
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        public Guid ImageId { get; set; }
+
+        public Image Image { get; set; }
+        
+        public ICollection<Article> Articles { get; set; }
     }
 }
