520b1af517b841fbc772e2b3bbc22091f416208f
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-03-12 22:21:12 +0300

    AuthController eklendi

---
+++ Blog.web/Areas/Admin/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.web.Areas.Admin.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
