c4e5315d01c3d9b1933d30a8e50c5ac8e3d9a201
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-03-12 22:24:33 +0300

    userManager ve signInManager fieldlerı oluşturuldu

--- Blog.web/Areas/Admin/Controllers/AuthController.cs
+++ Blog.web/Areas/Admin/Controllers/AuthController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Entity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.web.Areas.Admin.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+
+        public AuthController (UserManager<AppUser>userManager,SignInManager<AppUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
         public IActionResult Index()
         {
             return View();
