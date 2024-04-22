c84559c102536535d9387f7306039bdeb7ee2025
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-04-22 23:36:05 +0300

    UserLoginDto proplar eklendi

--- Blog.Entity/DTOs/Users/UserLoginDto.cs
+++ Blog.Entity/DTOs/Users/UserLoginDto.cs
@@ -8,5 +8,10 @@ namespace Blog.Entity.DTOs.Users
 {
     public class UserLoginDto
     {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
     }
 }
