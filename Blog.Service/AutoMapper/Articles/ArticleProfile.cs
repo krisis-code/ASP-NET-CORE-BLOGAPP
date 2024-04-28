6c260231145562535231824ac86ccd3832c6a051
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-04-28 22:35:24 +0300

    ArticleUpdateDto maplendi

--- Blog.Service/AutoMapper/Articles/ArticleProfile.cs
+++ Blog.Service/AutoMapper/Articles/ArticleProfile.cs
@@ -9,6 +9,7 @@ namespace Blog.Service.AutoMapper.Articles
         public ArticleProfile()
         {
             CreateMap<ArticleDto,Article>().ReverseMap();
+            CreateMap<ArticleUpdateDto, Article>().ReverseMap();
         }
     }
 }
