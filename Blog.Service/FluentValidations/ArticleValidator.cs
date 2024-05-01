a6921368031243e4c8f5ad5bee61c71a230ac7ef
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-01 10:59:21 +0300

    ArticleValidator title ve content için validate eklendi

--- Blog.Service/FluentValidations/ArticleValidator.cs
+++ Blog.Service/FluentValidations/ArticleValidator.cs
@@ -10,6 +10,21 @@ namespace Blog.Service.FluentValidations
 {
     public class ArticleValidator : AbstractValidator<Article>
     {
+        public ArticleValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .NotNull()
+                .MinimumLength(3)
+                .MaximumLength(150)
+                .WithName("Başlık");
 
+            RuleFor(x => x.Content)
+                 .NotEmpty()
+                 .NotNull()
+                 .MinimumLength(3)
+                 .MaximumLength(150)
+                 .WithName("İçerik");
+        }
     }
 }
