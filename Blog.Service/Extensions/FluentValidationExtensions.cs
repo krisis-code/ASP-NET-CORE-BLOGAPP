e1922b989a7c17b3d38351ac4d67622876deede6
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-01 11:16:58 +0300

    FluentValidationExtensions class ı yazıldı

--- Blog.Service/Extensions/FluentValidationExtensions.cs
+++ Blog.Service/Extensions/FluentValidationExtensions.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Blog.Service.Extensions
 {
-    internal class FluentValidationExtensions
+    public static class FluentValidationExtensions
     {
+        public static void AddToModelState(this ValidationResult result, ModelStateDictionary modelState)
+        {
+            foreach (var error in result.Errors)
+            {
+                modelState.AddModelError(error.PropertyName, error.ErrorMessage);
+            }
+        }
+
     }
 }
