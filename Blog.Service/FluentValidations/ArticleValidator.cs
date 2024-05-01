e5b1aa504f257be0f1a078ddb360915e924f52ff
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-01 10:55:48 +0300

    ArticleValidator class ı eklendi

---
+++ Blog.Service/FluentValidations/ArticleValidator.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Entity.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Service.FluentValidations
+{
+    public class ArticleValidator : AbstractValidator<Article>
+    {
+
+    }
+}
