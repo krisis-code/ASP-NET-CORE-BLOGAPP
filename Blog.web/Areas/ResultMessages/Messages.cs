65e1f1a6e3c73fdc9d7de45df6f11b8709e2b303
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-02 20:57:38 +0300

    Messages classına Add ve update metodu eklendi

--- Blog.web/Areas/ResultMessages/Messages.cs
+++ Blog.web/Areas/ResultMessages/Messages.cs
@@ -1,6 +1,18 @@
 ﻿namespace Blog.web.Areas.ResultMessages
 {
-    public class Messages
+    public static class Messages
     {
+        public static class Article
+        {
+            public static string Add(string articleTitle)
+            {
+                return $"{articleTitle} başlıklı makale başarıyla eklenmiştir.";
+            }
+
+            public static string Update(string articleTitle)
+            {
+                return $"{articleTitle} başlıklı makale başarıyla güncellenmiştir.";
+            }
+        }
     }
 }
