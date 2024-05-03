364a431471be0386172d157dd49cd99000980c84
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-03 19:47:46 +0300

    LoggedInUserExtension class ı dolduruldu

--- Blog.Service/Extensions/LoggedInUserExtension.cs
+++ Blog.Service/Extensions/LoggedInUserExtension.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Blog.Service.Extensions
 {
-    internal class LoggedInUserExtension
+    public static class LoggedInUserExtension
     {
+        public static Guid GetLoggedInUserId(this ClaimsPrincipal principal)
+        {
+            return Guid.Parse(principal.FindFirstValue(ClaimTypes.NameIdentifier));
+        }
+        public static string GetLoggedInUserEmail(this ClaimsPrincipal principal)
+        {
+            return principal.FindFirstValue(ClaimTypes.Name);
+        }
+
     }
 }
