d911c75eac25228c964024c90632edb15fd440d4
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-06 21:37:07 +0300

    Category index eklendi

--- Blog.web/Areas/Admin/Controllers/CategoryController.cs
+++ Blog.web/Areas/Admin/Controllers/CategoryController.cs
@@ -4,7 +4,7 @@ namespace Blog.web.Areas.Admin.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             return View();
         }
