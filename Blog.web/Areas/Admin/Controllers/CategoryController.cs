be080993d93ed63042514d7952626865c50ddf01
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-06 21:45:14 +0300

    Index metodu dolduruldu

--- Blog.web/Areas/Admin/Controllers/CategoryController.cs
+++ Blog.web/Areas/Admin/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Service.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.web.Areas.Admin.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ICategoryService categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
         public async Task<IActionResult> Index()
         {
-            return View();
+            var categories = await categoryService.GetAllCategoriesNonDeleted();
+            return View(categories);
         }
     }
 }
