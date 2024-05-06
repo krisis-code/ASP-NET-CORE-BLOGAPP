ca87caa55ef009bc0b9cef4a169b4e32c3f512cf
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-06 21:49:12 +0300

    viewler eklendi

--- Blog.web/Areas/Admin/Controllers/CategoryController.cs
+++ Blog.web/Areas/Admin/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.web.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly ICategoryService categoryService;
