fbff967458d8cbbffb050d8447042ba7221b1119
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-17 23:52:48 +0300

    userdto mapleme işlemi yapıldı

---
+++ Blog.Service/AutoMapper/Users/UsersProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Blog.Entity.DTOs.Users;
+using Blog.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Service.AutoMapper.Users
+{
+    public class UsersProfile : Profile
+    {
+        public UsersProfile() { 
+        
+            CreateMap<AppUser,UserDto>().ReverseMap();
+        }
+    }
+}
