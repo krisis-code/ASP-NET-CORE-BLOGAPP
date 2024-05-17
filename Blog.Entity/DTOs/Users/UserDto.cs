123beb998a86fbc99fbf86eccf8bb0cfb3ea33be
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-17 23:33:44 +0300

    UserDto proplar eklendi

--- Blog.Entity/DTOs/Users/UserDto.cs
+++ Blog.Entity/DTOs/Users/UserDto.cs
@@ -8,5 +8,19 @@ namespace Blog.Entity.DTOs.Users
 {
 	public class UserDto
 	{
-	}
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+        
+        public bool EmailConfirmed { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public int AccesFailedCount { get; set; }
+
+        public string Role { get; set; }
+    }
 }
