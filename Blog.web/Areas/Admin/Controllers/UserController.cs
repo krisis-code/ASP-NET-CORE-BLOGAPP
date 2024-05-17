afd4c62bafc0021083857dbec4cf30c20b82284a
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-17 23:29:22 +0300

    Users listeleme metodu yazıldı

--- Blog.web/Areas/Admin/Controllers/UserController.cs
+++ Blog.web/Areas/Admin/Controllers/UserController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Entity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.web.Areas.Admin.Controllers
 {
 	[Area("Admin")]
 	public class UserController : Controller
 	{
-		public async Task  <IActionResult> Index()
+		private readonly UserManager<AppUser> userManager;
+
+		public UserController(UserManager<AppUser> userManager)
+        {
+			this.userManager = userManager;
+		}
+        public async Task  <IActionResult> Index()
 		{
+			var users = await userManager.Users.ToListAsync();
 			return View();
 		}
 	}
