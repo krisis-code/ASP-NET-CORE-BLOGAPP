b8f8e64a154e7cacb6a00cf08a5042dc92f621aa
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-20 20:44:27 +0300

    UserAddDto proplar eklendi

--- Blog.Entity/DTOs/Users/UserAddDto.cs
+++ Blog.Entity/DTOs/Users/UserAddDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blog.Entity.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,17 @@ namespace Blog.Entity.DTOs.Users
 {
     public class UserAddDto
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public Guid RoleId { get; set; }
+
+        public List<AppRole> Roles { get; set; }
+
     }
 }
