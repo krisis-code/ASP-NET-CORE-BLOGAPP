0351f6c2c1ad6f85c701136f888accffc29c5d4a
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-27 22:37:46 +0300

    UserUpdateDto proplar eklendi

--- Blog.Entity/DTOs/Users/UserUpdateDto.cs
+++ Blog.Entity/DTOs/Users/UserUpdateDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blog.Entity.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,19 @@ namespace Blog.Entity.DTOs.Users
 {
     public class UserUpdateDto
     {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Password { get; set; }
+
+        public Guid RoleId { get; set; }
+
+        public List<AppRole> Roles { get; set; }
     }
 }
