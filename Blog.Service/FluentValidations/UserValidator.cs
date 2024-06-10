c36fc0130792bc65b22b4f1e01cc7e0d83d5aa4e
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-06-10 23:01:59 +0300

    validateler eklendi

--- Blog.Service/FluentValidations/UserValidator.cs
+++ Blog.Service/FluentValidations/UserValidator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Blog.Entity.Entities;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,28 @@ using System.Threading.Tasks;
 
 namespace Blog.Service.FluentValidations
 {
-    public class UserValidator
+    public class UserValidator : AbstractValidator<AppUser>
     {
+        public UserValidator()
+        {
+            RuleFor(x => x.FirstName)
+               .NotEmpty()
+               .NotNull()
+               .MinimumLength(3)
+               .MaximumLength(50)
+               .WithName("İsim");
+            RuleFor(x => x.LastName)
+              .NotEmpty()
+              .NotNull()
+              .MinimumLength(3)
+              .MaximumLength(50)
+              .WithName("Soyisim");
+
+            RuleFor(x => x.PhoneNumber)
+            .NotEmpty()
+            .NotNull()
+            .MinimumLength(11)
+            .WithName("Telefon Numarası");
+        }
     }
 }
