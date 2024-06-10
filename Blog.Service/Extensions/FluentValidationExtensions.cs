f61ff83f320723d14e232c6866d804d5f97ede38
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-06-10 23:38:19 +0300

    FluentValidationExtensions a AddToIdentityModelState metodu eklendi

--- Blog.Service/Extensions/FluentValidationExtensions.cs
+++ Blog.Service/Extensions/FluentValidationExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Blog.Service.Extensions
@@ -10,6 +11,13 @@ namespace Blog.Service.Extensions
             foreach (var error in result.Errors)
             {
                 modelState.AddModelError(error.PropertyName, error.ErrorMessage);
+            }
+        }
+        public static void AddToIdentityModelState(this IdentityResult result, ModelStateDictionary modelState)
+        {
+            foreach (var error in result.Errors)
+            {
+                modelState.AddModelError("", error.Description);
             }
         }
 
