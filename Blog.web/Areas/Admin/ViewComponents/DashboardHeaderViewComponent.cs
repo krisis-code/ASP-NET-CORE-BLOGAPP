965ebe6fdde28dadcb0f54204ab553f74154628a
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-09-15 17:01:00 +0300

    Kullanıcı profili kısmı viewcomponents olarak ayrıldı ve listelendi

---
+++ Blog.web/Areas/Admin/ViewComponents/DashboardHeaderViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Blog.Entity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.web.Areas.Admin.ViewComponents
+{
+    public class DashboardHeaderViewComponent : ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public DashboardHeaderViewComponent(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var loggedInUser = await _userManager.GetUserAsync(HttpContext.User);
+            return View(loggedInUser);
+        }
+    }
+}
