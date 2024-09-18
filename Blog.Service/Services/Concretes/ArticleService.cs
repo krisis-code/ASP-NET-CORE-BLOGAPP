df73fb27db1adc7d9c2f8e47e668262109d0a0bf
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-09-18 19:03:35 +0300

    Pagination pagesize bug çözüldü

--- Blog.Service/Services/Concretes/ArticleService.cs
+++ Blog.Service/Services/Concretes/ArticleService.cs
@@ -34,24 +34,23 @@ namespace Blog.Data.Repositories.Concretes
         public async Task<ArticleListDto> GetAllByPagingAsync(Guid? categoryId,int currentPage = 1,int pageSize = 3,bool isAscending = false)
         {
             pageSize = pageSize > 20 ? 20 : pageSize;
-
             var articles = categoryId == null
-                ? await unitOfWork.GetRepository<Article>().GetAllAsync(a => !a.IsDeleted, a => a.Category, i => i.Image,u=>u.User)
-                : await unitOfWork.GetRepository<Article>().GetAllAsync(a => a.CategoryId == categoryId && !a.IsDeleted, x => x.Category, i => i.Image, u => u.User);
-
-            var sortedArticle = isAscending
-                ? articles.OrderBy(x => x.CreatedDate).Skip((currentPage - 1) * pageSize).ToList()
-                : articles.OrderByDescending(x => x.CreatedDate).Skip((currentPage - 1) * pageSize).ToList();
-
-            return new ArticleListDto 
+                ? await unitOfWork.GetRepository<Article>().GetAllAsync(a => !a.IsDeleted, a => a.Category, i => i.Image, u => u.User)
+                : await unitOfWork.GetRepository<Article>().GetAllAsync(a => a.CategoryId == categoryId && !a.IsDeleted,
+                    a => a.Category, i => i.Image, u => u.User);
+            var sortedArticles = isAscending
+                ? articles.OrderBy(a => a.CreatedDate).Skip((currentPage - 1) * pageSize).Take(pageSize).ToList()
+                : articles.OrderByDescending(a => a.CreatedDate).Skip((currentPage - 1) * pageSize).Take(pageSize).ToList();
+            return new ArticleListDto
             {
-                Articles = sortedArticle ,
-                CategoryId = categoryId == null? null:categoryId.Value,
+                Articles = sortedArticles,
+                CategoryId = categoryId == null ? null : categoryId.Value,
                 CurrentPage = currentPage,
                 PageSize = pageSize,
                 TotalCount = articles.Count,
                 IsAscending = isAscending
             };
+
         }
 
         public async Task CreateArticleAsync(ArticleAddDto articleAddDto)
